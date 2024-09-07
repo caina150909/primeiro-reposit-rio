e768168730de3b830966e1a358b60fea260005ef
caina150909 <cainadpchaves@gmail.com>
2024-09-07 13:17:21 -0300

    segundo commit

--- estudosgit.py
+++ estudosgit.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+print("---------------------VERIFICAÇÃO DE IDADE-----------------------")
+
+while True:
+    idade = int(input("\nDIGITE SUA IDADE: "))
+
+    if idade >= 18 and idade <= 120 or idade == -1:
+        break
+        print("Idade permitida: ")
+    else:
+        print("PROIBIDO IDADE INSUFICIENTE")   
+        
+    
